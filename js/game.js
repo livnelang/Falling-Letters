2a11989c0fad902bbe258f4635d0c5e12b1f9c54
Livne Lang <livnelang@gmail.com>
2017-06-10 11:20:14 +0300

    hebrew support

--- js/game.js
+++ js/game.js
@@ -20,7 +20,8 @@ function loadGame() {
  * @param start- true/false/null
  */
 function LettersGame(start) {
-    var LETTERS = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    // var LETTERS = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    var LETTERS = ['א','ב','ג','ד','ה','ו','ז','ח','ט','י','כ','ל','מ','נ','ס','ע','פ','צ','ק','ר','ש','ת'];
     var letters_array = {};
     var gameOn = true;
     var timeOffset = 2000; //interval between letters starting, will be faster over time
